2403633e22cdd5c9ae0b460a4fa68dc5b33a9eb4
PeterHyperAung <peteraungmpa2006@gmail.com>
2022-12-03 12:01:12 +0630

    3 december 2022

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 document.addEventListener("scroll", function () {
   const nav = document.getElementById("navbar");
-  if (window.scrollY > 700) {
+  const point = 630;
+  if (window.scrollY > point) {
     nav.classList.remove("navbar-backdrop");
     nav.classList.remove("navbar-dark");
     nav.classList.add("bg-white");
     nav.classList.add("navbar-white");
   }
-  if (window.scrollY <= 700) {
+  if (window.scrollY <= point) {
     nav.classList.remove("bg-white");
     nav.classList.remove("navbar-white");
     nav.classList.add("navbar-backdrop");
